44a929cae0a70976c4f1a76344275badb878fcaf
Gabrielly Maroli <403302@unoesc.lan>
2023-08-24 21:38:26 -0300

    att fixação

---
+++ attFixacao/PjGabriellyMaroli/PjGabriellyMaroli/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PjGabriellyMaroli.Models;
+
+namespace PjGabriellyMaroli.Data
+{
+    public class AppDbContext:
+        IdentityDbContext <Psicologo>
+    {
+        public AppDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Psicologo> Psicologos { get; set; }
+     }
+}
---
+++ attFixacao/PjGabriellyMaroli/PjGabriellyMaroli/Models/Psicologo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PjGabriellyMaroli.Models
+{
+    public class Psicologo : IdentityUser
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nome requerido")]
+        [Display(Name= "Nome completo")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "CRP requerido")]
+        [Display(Name = "CRP")]
+        public int CRP { get; set; }
+
+        [Required(ErrorMessage = "Endereço requerido")]
+        [Display(Name = "Endereço")]
+        public string? Endereco { get; set; }
+
+        [Required(ErrorMessage = "Data requerida")]
+        [Display(Name = "Data de nascimento")]
+        public DateTime? Nascimento { get; set; }
+    }
+
+}
